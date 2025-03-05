468327d4bdbfad8d431c1c9b8fb1d001994cc6c4
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-03-05 22:23:15 +0600

    Add files via upload

---
+++ sort & count.py
@@ -0,0 +1,11 @@
+"""WAP to count the number of students with the A grade in the following tuple.
+f=[C,D,A,A,B,B,A] and doing list & sort them from A to D."""
+
+e=("C","D","A","A","B","B","A")
+counting= e.count("A")
+print(counting)
+
+f=["C","D","A","A","B","B","A"]
+
+s_f=sorted(f)
+print(s_f)
